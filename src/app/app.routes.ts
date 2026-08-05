3a12aa6f6d79f468eed183f1e9ef5452f0a4bbde
Your Name <you@company.com>
2026-08-06 02:08:30 +0330

    fix: add name attributes to all ngModel fields in forms and remove unused routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: '**', redirectTo: '/' }
-];
+export const routes: Routes = [];
